8a9ac91518ccc875c4a5c5ae5b76cfbd02b8f8bf
Andy Chou <yhchou@91app.com>
2020-03-11 10:50:20 +0800

    feat: cancel useEffect, not storing local state

--- client/src/components/BookDetails.jsx
+++ client/src/components/BookDetails.jsx
@@ -1,10 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useLazyQuery } from '@apollo/react-hooks';
 import { GET_BOOK_QUERY } from '../queries/queries';
 
 function BookDetails({ booksList }) {
   const [selectedBookId, setSelectedBookId] = useState(null);
-  const [bookData, setBookData] = useState(null);
   const [getBookQuery, { error, data: bookQueryData }] = useLazyQuery(GET_BOOK_QUERY, {
     variables: { id: selectedBookId }
   });
@@ -12,19 +11,6 @@ function BookDetails({ booksList }) {
     setSelectedBookId(e.target.value);
     getBookQuery();
   };
-  useEffect(() => {
-    if (bookQueryData) {
-      setBookData({
-        name: bookQueryData.book.name,
-        genre: bookQueryData.book.genre,
-        author: bookQueryData.book.author.name,
-        authorAge: bookQueryData.book.author.age,
-        otherBooks: bookQueryData.book.author.books.filter(
-          book => book.id !== bookQueryData.book.id
-        )
-      });
-    }
-  }, [bookQueryData]);
   if (error) return <p>Error :(</p>;
   return (
     <div>
@@ -38,17 +24,19 @@ function BookDetails({ booksList }) {
         ))}
       </select>
       <div id="book-details">
-        {bookData ? (
+        {bookQueryData ? (
           <>
-            <p>Book Name: {bookData.name}</p>
-            <p>Book Genre: {bookData.genre}</p>
-            <p>Book Author: {bookData.author}</p>
-            <p>Book Author's age: {bookData.authorAge}</p>
+            <p>Book Name: {bookQueryData.book.name}</p>
+            <p>Book Genre: {bookQueryData.book.genre}</p>
+            <p>Book Author: {bookQueryData.book.author.name}</p>
+            <p>Book Author's age: {bookQueryData.book.author.age}</p>
             <p>Other Books from the Same Author:</p>
             <ul>
-              {bookData.otherBooks.map(book => (
-                <li key={book.id}>{book.name}</li>
-              ))}
+              {bookQueryData.book.author.books
+                .filter(book => book.id !== bookQueryData.book.id)
+                .map(book => (
+                  <li key={book.id}>{book.name}</li>
+                ))}
             </ul>
           </>
         ) : null}
